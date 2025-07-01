c0396dd6a096c3b9d0ad0d657e9305097421ffb6
Ricardo Daniel Ucan Ucan <159578063+RicardoUcan18@users.noreply.github.com>
2025-07-01 17:08:57 -0600

    Add files via upload

--- src/exercises/3-validacion-de-tipados/index.ts
+++ src/exercises/3-validacion-de-tipados/index.ts
@@ -1,10 +1,12 @@
 interface User {
+  type: 'User';
   name: string;
   age: number;
   occupation: string;
 }
 
 interface Admin {
+  type: 'Admin';
   name: string;
   age: number;
   role: string;
@@ -14,21 +16,25 @@ export type Person = User | Admin;
 
 export const persons: Person[] = [
   {
+    type:'User',
     name: 'Alvaro Castillo',
     age: 25,
     occupation: 'Ingeniero de Software',
   },
   {
+    type:'Admin',
     name: 'Javier Castillo',
     age: 32,
     role: 'Estudiante',
   },
   {
+    type:'User',
     name: 'Maria Isabel Hernandez',
     age: 23,
     occupation: 'Abogada',
   },
   {
+    type:'Admin',
     name: 'Jorge Perez',
     age: 64,
     role: 'Contador',
@@ -36,13 +42,14 @@ export const persons: Person[] = [
 ];
 
 export function logPerson(person: Person) {
-  let additionalInformation: string;
-  if (person.role) {
+  let additionalInformation: string = '';
+  if (person.type == 'Admin') {
     additionalInformation = person.role;
-  } else {
+  } else if (person.type == 'User') {
     additionalInformation = person.occupation;
   }
   console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
+
 
 persons.forEach(logPerson);
